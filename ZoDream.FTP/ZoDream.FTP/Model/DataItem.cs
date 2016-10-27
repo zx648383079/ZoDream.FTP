4147aeaade8513b9bafecd77f5f3556f7386c6e1
zx648383079 <648383079@qq.com>
2016-10-27 13:55:47 +0800

    初始化（页面设计中）

---
+++ ZoDream.FTP/ZoDream.FTP/Model/DataItem.cs
@@ -0,0 +1,16 @@
+﻿namespace ZoDream.FTP.Model
+{
+    public class DataItem
+    {
+        public string Title
+        {
+            get;
+            private set;
+        }
+
+        public DataItem(string title)
+        {
+            Title = title;
+        }
+    }
+}
